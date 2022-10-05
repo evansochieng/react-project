b4ed0efe205a6d9a9b6d4d29a715b8b2194b5605
Evans Ochieng <evansochieng121@gmail.com>
2022-10-05 14:48:04 +0300

    Display components through using app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
+// Import the other components
+import NavBar from "./components/NavBar";
+import SignIn from "./components/SignIn";
+import Home from "./components/Home";
+import CreateResume from "./components/CreateResume";
+import SignOut from "./components/SignOut";
 
 function App() {
   return (
-    
+    <div>
+      <NavBar />
+      <Routes>
+        <Route exact path="/signin" element={<SignIn />} />
+
+        <Route exact path="/" element={<Home />} />
+
+        <Route exact path="/createresume" element={<CreateResume />} />
+
+        <Route exact path="/signout" element={<SignOut />} />
+      </Routes>
+    </div>
   );
 }
 
