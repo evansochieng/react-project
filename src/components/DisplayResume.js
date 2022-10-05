d035bb49544e34ccb41232c4da940c8537e4ea4d
Evans Ochieng <evansochieng121@gmail.com>
2022-10-05 19:27:34 +0300

    Able to display resume in the server

--- src/components/DisplayResume.js
+++ src/components/DisplayResume.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function DisplayResume(resume) {
+function DisplayResume({resume}) {
   return (
     <div>
         <h2>{resume.name}</h2>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import DisplayResume from "./DisplayResume";
 
-function Home({ resumes }) {
+function Home() {
+    //define state for search item
+    const [searchedName, setSearchedName] = useState('');
 
-    // fetch the data from the api depending on the user input
+    //define state for matching user
+    const [matchingUser, setMatchingUser] = useState('');
+
+    // set searched name from user input
+    function grabUserInput(event){
+        const name = event.target.value;
+        setSearchedName(name);
+    }
+
+    function searchResume(event){
+        event.preventDefault();
+
+        {
+          /* fetch the data from the api depending on the user input */
+        }
+        fetch("http://localhost:8000/resumes")
+        .then(resp => resp.json())
+        .then(resumes => {
+          // Match searched user with appropriate resume
+          const searchedUser = resumes.find(
+            (resume) =>
+              resume.name.split(" ").join("").toLowerCase() ===
+              searchedName.split(" ").join("").toLowerCase()
+          );
+
+          setMatchingUser(searchedUser);
+          
+          //Reset search box and matchingUser
+          setSearchedName('');
+          setMatchingUser('');
+        })
+    }
     
     return (
       <>
         <h1>Welcome to Resume Builder Application</h1>
-
-        <form className="search-form">
-          <input type="text" placeholder="search resume" />
+        <form onSubmit={(e) => searchResume(e)}>
+          <input
+            type="text"
+            placeholder="search resume"
+            value={searchedName}
+            onChange={(e) => grabUserInput(e)}
+          />
           <button>Search</button>
         </form>
 
         <div>
-          <p>Your resume will appear here</p>
+          {matchingUser ? (
+            <DisplayResume resume={matchingUser} />
+          ) : (
+            <p>
+              <strong>You have no resume yet. Please create one...</strong>
+            </p>
+          )}
         </div>
       </>
     );
