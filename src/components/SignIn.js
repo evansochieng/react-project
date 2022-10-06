1bfed6ad650ab5a54a76ec2f8b47262ab5c374bc
Evans Ochieng <evansochieng121@gmail.com>
2022-10-06 08:51:35 +0300

    Added programmatic navigation functionality

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SignIn() {
+  // Define navigate variable to assist you navigate programmatically
+  const navigate = useNavigate();
+  
+  // Define state for user logins
+  const [userLogins, setUserLogins] = useState({
+    username: '',
+    password: ''
+  })
+
+  // Handle change of logins
+  function handleUserLogins(e){
+    setUserLogins({
+      ...userLogins,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  // Function to login user
+  function loginUser(event){
+    event.preventDefault();
+    navigate('/')
+  }
+
   return (
     <div>
-      <form className="flex-1" style={{display: 'flex', flexDirection: 'column', width: '400px'}}>
+      <form
+        style={{ display: "flex", flexDirection: "column", width: "400px" }}
+        onSubmit={(e) => loginUser(e)}
+      >
         <label htmlFor="username">Username: </label>
-        <input type="text" name="username" id="username" />
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={userLogins.username}
+          onChange={(e) => handleUserLogins(e)}
+        />
 
         <label htmlFor="password">Password: </label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={userLogins.password}
+          onChange={(e) => handleUserLogins(e)}
+        />
 
-        <button style={{width: '100px'}}>Sign In</button>
+        <button style={{ width: "100px" }}>Sign In</button>
       </form>
     </div>
   );
--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function SignOut() {
+  // Define navigate to assist go back to login on logout
+  const navigate = useNavigate();
+
+  //Log out user, redirect to login
+  function logout(){
+    navigate("/signin");
+  }
   return (
     <>
-      <h3>Signing out...</h3>
+      <h3>Confirm signout</h3>
+      <button onClick={logout}>SignOut</button>
     </>
   );
 }
