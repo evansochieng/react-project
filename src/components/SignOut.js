9a316841a39577442ee254f8e920febf32ab58b4
Evans Ochieng <evansochieng121@gmail.com>
2022-10-06 13:32:46 +0300

    Styled sign out page

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { ImSad } from 'react-icons/im'
 
 function SignOut() {
   // Define navigate to assist go back to login on logout
@@ -9,17 +10,53 @@ function SignOut() {
   function logout(){
     navigate("/signin");
   }
+
+  // Return user to home
+  function redirectHome(){
+    navigate("/")
+  }
+
   return (
     <div
       style={{
-        display: "inline-block",
-        textAlign: 'center',
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
         padding: "40px",
         width: "1000px",
       }}
     >
-      <h3>Confirm signout</h3>
-      <button onClick={logout}>SignOut</button>
+      <h3 style={{ color: "#fb923c" }}>I am sad to see you go</h3>
+      <p>Are you sure you want to sign out?</p>
+      <ImSad style={{ color: "#fcd34d", fontSize: '50px' }} />
+      <button
+        style={{
+          width: "200px",
+          height: "30px",
+          backgroundColor: "#10b981",
+          color: "white",
+          borderRadius: "5px",
+          borderColor: "none",
+          marginTop: '10px',
+          marginBottom: '10px'
+        }}
+        onClick={logout}
+      >
+        YES, sign out
+      </button>
+      <button
+        style={{
+          width: "200px",
+          height: "30px",
+          borderRadius: "5px",
+          borderColor: "none",
+        }}
+        onClick={redirectHome}
+      >
+        Cancel
+      </button>
     </div>
   );
 }
